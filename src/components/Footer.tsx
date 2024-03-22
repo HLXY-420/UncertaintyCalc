0c0edeb034cddd696ceaca20738febffc2b83450
HLXY <hlxy420@outlook.com>
2024-03-22 09:53:34 +0800

    using react as base and build the formula input via cortex.js

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>
+                111
+            </p>
+        </footer>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Uncertainty Calc</h1>
+        </header>
+    )
+}
