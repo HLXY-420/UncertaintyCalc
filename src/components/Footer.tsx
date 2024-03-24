5220530c469c88c32287010958e90827f0005181
HLXY <hlxy420@outlook.com>
2024-03-24 17:27:24 +0800

    update the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,10 @@ export default function Footer() {
     return (
         <footer>
             <p>
-                Made by <a href="https://github.com/HLXY-420" target="_blank">HLXY</a>. See the <a href="https://github.com/HLXY-420/UncertaintyCalc/" target="_blank"> source code.</a>
+                Made by <a href="https://github.com/HLXY-420" target="_blank">HLXY</a>. See the <a href="https://github.com/HLXY-420/UncertaintyCalc/" target="_blank"> source code</a> under the MIT license.
+            </p>
+            <p>
+                <a href="https://github.com/HLXY-420/UncertaintyCalc/issues" target="_blank">Report an issue</a>
             </p>
         </footer>
     )
